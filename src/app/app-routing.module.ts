4ccf8b32b13935f3e959f537346fbe253963880f
varshith2703 <varshithnaidubuddana@gmail.com>
2024-04-30 00:58:08 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BusDetailsComponent } from './pages/bus-details/bus-details.component';
+import { SelectSeatsComponent } from './pages/select-seats/select-seats.component';
+import { BillComponent } from './pages/bill/bill.component';
+import { BookingComponent } from './pages/bookings/bookings.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'bus-details', component: BusDetailsComponent },
+  { path: 'select-seats', component: SelectSeatsComponent},
+  { path: 'bill', component: BillComponent},
+  { path: 'booking', component: BookingComponent} // Corrected path name
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BusDetailsComponent } from './pages/bus-details/bus-details.component';
+import { SelectSeatsComponent } from './pages/select-seats/select-seats.component';
+import { BillComponent } from './pages/bill/bill.component';
+import { BookingComponent } from './pages/bookings/bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BusDetailsComponent,
+    SelectSeatsComponent,
+    BillComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
